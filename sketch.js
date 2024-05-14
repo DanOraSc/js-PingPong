32b929c546db990b7a27107358131c163d2d3fc0
DanOraSc <scramindanilo@gmail.com>
2024-05-14 12:18:05 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,22 @@
-//variáveis da bolinha
 let xBolinha = 100;
 let yBolinha = 200;
 let diametro = 20;
 let raio = diametro / 2;
 
-//variáveis do oponente
 let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 
-//velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
-//variáveis da raquete
 let xRaquete = 5;
 let yRaquete = 150;
 let raqueteComprimento = 10;
 let raqueteAltura = 90;
 
-//placar do jogo
 let meusPontos = 0;
 let pontosDoOponente = 0;
 
-//sons do jogo
 let raquetada;
 let ponto;
 let trilha;
@@ -115,9 +109,6 @@ function incluiPlacar(){
     rect(450, 10, 40, 20);
     fill(255);
     text(pontosDoOponente, 470, 26);
-
-
-
 }
 
 
